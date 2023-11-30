1fb9727272bbd3ec055a223c7e33f56d0b60e93a
ATOUI Abderahman Yakoub <117508580+ATOUIYakoub@users.noreply.github.com>
2023-11-30 22:13:21 +0100

    adding rate meal fonction

--- api/views.py
+++ api/views.py
@@ -2,10 +2,51 @@ from rest_framework import viewsets
 from .models import Meal, Rating
 from .serializers import MealSerializer, RatingSerializer
 
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+
+
 # Create your views here.
 class MealViewsets(viewsets.ModelViewSet):
     queryset = Meal.objects.all()
     serializer_class = MealSerializer
+    
+    @action(method='POST',detail=True)
+    def rate_meal(self, REQUEST, pk=None):
+        if 'stars' in request.data:
+            '''create or update'''
+            meal = Meal.objects.get(id=pk)
+            username = request.data['username']
+            stars = request.data['stars']
+            user = User.objects.get(username=username)
+            
+            try:
+                #update
+                rate = Rating.objects.get(user=user.id , meal=meal.id)
+                rating.stars = stars
+                rating.save
+                serializer = RatingSerializer(rating, many=False)
+                json = {
+                    'meassage':'meal Rate updated',
+                    'result':serializer.data
+                }
+                return Response(json, status=status.HTTP_400_BAD_REQUEST)
+            except:
+                #create
+                rating = Rating.objects.create(stars=stars,meal=meal,user=user)
+                serializer = RatingSerializer(rating, many=False)
+                json = {
+                    'meassage':'meal Rate created',
+                    'result':serializer.data
+                }
+                return Response(json, status=status.HTTP_200_OK)
+            
+        else:
+            json = {
+                'message':'stars not provided'
+            }
+            return Response(json, status=status.HTTP_400_BAD_REQUEST)
     
 class RatingViewsets(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
